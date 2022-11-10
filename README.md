582631735654e03cc0dd29c29cdcf7f04b3b1b45
Andreza Barros Tiezzi <107008175+AndrezaBarros@users.noreply.github.com>
2022-11-10 14:31:22 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+# Git Fav
+
+Lista de usuários favoritos do Github.
+<br>
+
+![preview](./assets/preview.png)
+
+## ⚒️ Tecnologias
+
+- HTML
+- CSS
+- JavaScript
+- Github
+
+<br>
+
+## 📖 Pontos abordados - JavaScript
+* Classes
+* Heranças (extends - super)
+* Eventos
+* Desestruturação 
+* Local Storage
+* Utilização de API
+* Try Catch
+
+<br>
+
+## ⚙️ Funcionalidades
+* Busca de usuário pelo login
+* Apresentação de dados reais do usuário
+* Inclusão e exclusão de usuários da lista
+* Mensagem de erro, caso o usuário não seja encontrado
+* Mensagem de confirmação para remoção do usuário
+* Tela de aviso de lista vazia
---
+++ js/favorites.js
@@ -0,0 +1,135 @@
+import { GithubUser } from "./githubUser.js"
+
+export class Favorites {
+    constructor(root) {
+        this.root = document.querySelector(root)
+        this.load()
+    }
+
+    load() {
+        this.entries = JSON.parse(localStorage.getItem('@git-fav')) || []
+    }
+
+    save() {
+        localStorage.setItem('@git-fav',JSON.stringify(this.entries))
+    }
+
+    delete(user) {
+        const filteredEntries = this.entries.filter(entry => entry.login != user.login)
+
+        this.entries = filteredEntries
+
+        this.update()
+        this.save()
+    }
+
+    async add(username) {
+        try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado')
+            }
+
+            const user = await GithubUser.searchUser(username)
+
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch(error) {
+            alert(error.message)
+        }
+        
+    }
+}
+
+export class FavoritesView extends Favorites {
+    constructor(root) {
+        super(root)
+
+        this.tbody = this.root.querySelector("table tbody")
+
+        this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.searchUser button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.searchUser input')
+
+            this.add(value)
+        }
+    }
+
+    update() {
+        this.removeAllTr()
+        
+        this.entries.forEach( user => {
+            const row = this.creatRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar esta linha?')
+
+                if(isOk) {
+                    this.delete(user)
+                }
+            }
+
+            this.tbody.append(row)
+            this.removeAlertEmptyTable()
+        })
+    }
+
+    creatRow() {
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+            <td class="user">
+                <img src="https://github.com/AndrezaBarros.png" alt="Imagem do usuário">
+                <a href="">
+                    <p>Andreza Barros Tiezzi</p>
+                    <span>/AndrezaBarros</span>
+                </a>
+            </td>
+
+            <td class="repositories">123</td>
+            <td class="followers">1234</td>
+            <td>
+                <button class="remove">Remover</button>
+            </td>
+        `
+        return tr
+    }
+
+    addAlertEmptyTable() {
+        let emptyTable = this.root.querySelector('.emptyTable')
+        emptyTable.classList.remove('hide')
+    }
+
+    removeAlertEmptyTable() {
+        let emptyTable = this.root.querySelector('.emptyTable')
+        emptyTable.classList.add('hide')
+    }
+
+    removeAllTr() {
+        this.tbody.querySelectorAll('tr').forEach(tr => {
+            tr.remove()
+            this.addAlertEmptyTable()
+        })
+    }
+}
